bdd9152a746d191b978e337b0a1af115c1806e1e
Navarjun <navarjunsingh@gmail.com>
2017-03-28 23:43:01 -0400

    Complete JS

--- script-complete.js
+++ script-complete.js
@@ -1,15 +1,42 @@
 var highlighterDiv = document.getElementById('highlighter');
 var triggers = document.querySelectorAll("div img");
 
-for (var i = 0; i < triggers.length; i++) {
-  trigger = triggers[i];
-  trigger.addEventListener("mouseenter", function(e) {
-    var coords = this.getBoundingClientRect();
-    highlighterDiv.style.top = coords.top+"px";
-    highlighterDiv.style.left = coords.left+"px";
-
-    highlighterDiv.style.width = coords.width+"px";
-    highlighterDiv.style.height = coords.height+"px";
-    highlighterDiv.style.opacity = 1;
+imagesArray.forEach(function(d, i) {
+  var image = d;
+  image.addEventListener("mouseover", function(){
+    var rect = this.getBoundingClientRect();
+    currentIndex = i;
+    updateHighlighter();
   });
+});
+
+function updateHighlighter() {
+  var image = imagesArray[currentIndex];
+
+  var rect = image.getBoundingClientRect();
+  highlighter.style.opacity = 1;
+  highlighter.style.width = rect.width+"px";
+  highlighter.style.height = rect.height+"px";
+  highlighter.style.top = rect.top+"px";
+  highlighter.style.left = rect.left+"px";
 }
+
+window.addEventListener("keydown", function(e) {
+  console.log(e.keyCode);
+
+  if (e.keyCode == 37) {
+    // left arrow
+    currentIndex--;
+  } else if (e.keyCode == 39) {
+    // right arrow
+    currentIndex++;
+  }
+
+  if (currentIndex >= imagesArray.length) {
+    currentIndex = 0;
+  } else if (currentIndex < 0) {
+    currentIndex = imagesArray.length -1;
+  }
+
+  updateHighlighter();
+});
